992b305bf42bef87f86e96a2b4a6d1a10ba7ea0f
LiNk-NY <mramos09@gmail.com>
2016-11-21 21:23:20 -0500

    read exonfiles and subsetByOverlaps

--- R/readExonFiles.R
+++ R/readExonFiles.R
@@ -1,8 +1,34 @@
 library(BiocInterfaces)
 library(readr)
 
-exonFiles <- list.files("/data/exon/", recursive = TRUE,
-                        pattern = "tion.txt$", full.names = TRUE)
-exonFiles
+source("R/parsePRADmetadata.R")
 
-TCGAexonToGRangesList(exonFiles)
+exonFiles <- list.files("/data/exon/", recursive = TRUE, pattern = "tion.txt$", full.names = TRUE)
+folders <- basename(dirname(exonFiles))
+
+validIdx <- match(validMap$file_id, folders)
+
+validFoldersOnDisk <- folders[validIdx]
+exonFiles <- exonFiles[validIdx]
+
+orderIdx <- order(validFoldersOnDisk)
+
+validFoldersOnDisk <- validFoldersOnDisk[orderIdx]
+exonFiles <- exonFiles[orderIdx]
+
+
+validMap <- validMap[order(validMap$file_id),]
+
+stopifnot(identical(validMap$file_id, validFoldersOnDisk))
+stopifnot(identical(basename(dirname(exonFiles)), validMap$file_id))
+
+grl <- TCGAexonToGRangesList(exonFiles, filenames = validMap$barcode)
+
+Exon9s <- lapply(grl, function(sample) {
+    sample[overlapsAny(sample, exon9, type = "any")]
+})
+
+saveRDS(Exon9s, file = "data-raw/exon9grl.rds")
+
+rm(grl)
+gc()
